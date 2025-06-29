f84df53429dd80da3272ba71a1ddd5721ca8412e
AnuBaswaraj <banushka1591@gmail.com>
2025-06-29 23:29:55 +0530

    Update Register.jsx

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -33,7 +33,7 @@ const Register = () => {
     console.log(credentials);
 
     try {
-      const response = await fetch(`${baseUrl}/register`, {
+      const response = await fetch(`${baseUrl}/api/auth/register`, {
         method: "POST",
         body: formData
       });
