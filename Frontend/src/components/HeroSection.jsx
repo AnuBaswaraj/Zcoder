6ff1aca2c1dbe19b47f50ed20624b91602c4bf38
AnuBaswaraj <banushka1591@gmail.com>
2025-06-29 21:33:34 +0530

    Update HeroSection.jsx

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -14,7 +14,7 @@ const HeroSection = () => {
     <div className="flex flex-col items-center mt-6 lg:mt-20">
       
     <h1 className="text-4wl sm:text-6xl lg:text-7xl text-center tracking-wide">
-        All Your Coding Challanges
+        All Your Coding Challenges
         
         <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
         {" "}  
